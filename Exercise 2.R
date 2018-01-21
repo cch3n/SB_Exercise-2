3bd7d03c41f48428119f226959fcad2a49d45aa1
cch3n <34888938+cch3n@users.noreply.github.com>
2018-01-21 18:49:40 -0500

    Add files via upload

---
+++ Exercise 2.R
@@ -0,0 +1,22 @@
+library(tidyr)
+library(dplyr)
+
+titanic <- read.csv("titanic_original.csv")
+
+# Port of Embarkation: Fill NA values in 'Embarked" with S
+titanic$embarked[is.na(titanic$embarked)] <- "S"
+
+#Age: Replaces missing values with average age
+titanic <- mutate(titanic, age = if_else(is.na(age), mean(age, na.rm = TRUE), age))
+
+#Lifeboat Replaces those without lifeboats with "None"
+titanic <- mutate(titanic, boat = ifelse(is.na(boat), "None", boat))
+
+#Cabin, filling in missing values could help provide more information regarding passengers. 
+#A missing cabin number could mean that the individual was not on the ship ledger. 
+#It would still be helpful to note if that is the case. 
+titanic <- mutate(titanic, has_cabin_number = ifelse(is.na(cabin), 0, 1))
+
+View(titanic)
+
+write.csv(titanic_original, file = "titanic_clean.csv")
